3a4eb38a0f1b7eb48e57425069bebc8b55321a1c
zin-min-phyo-kozin <s6730042220@northcm.ac.th>
2025-11-23 14:04:23 +0700

    Add Config struct and NewConfig function

---
+++ internal/config/config.go
@@ -0,0 +1,11 @@
+package config
+
+// Config represents the configuration structure.
+type Config struct {
+    // Add your configuration fields here
+}
+
+// NewConfig creates a new instance of Config.
+func NewConfig() *Config {
+    return &Config{}
+}
